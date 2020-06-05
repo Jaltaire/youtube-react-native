31ff9004672db42d55b3537ce9a7dd93534f062a
Jonathan Alter <jaltaire@gmail.com>
2020-06-04 23:22:40 -0400

    Basic YouTube search and streaming app with React Native.

---
+++ services/youtube-api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import env from '../env.json';
+
+const API_URL = env.apiURL;
+const API_KEY = env.apiKey;
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+    maxResults: 15,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
